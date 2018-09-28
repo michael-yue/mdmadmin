dc0f0c2aa28be27e05fd423ab3fe839ad9d90f5e
michael-yue <michael_yue@163.com>
2018-09-28 14:46:20 +0800

    commit1

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -55,7 +55,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: true,
       favicon: resolve('favicon.ico'),
-      title: 'vue-admin-template',
+      title: '后台管理',
       minify: {
         removeComments: true,
         collapseWhitespace: true,
