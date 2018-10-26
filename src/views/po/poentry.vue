661c29746ad87132ec3fc06d9e6a38c7ecd82505
michael <michael_yue@163.com>
2018-10-26 17:10:44 +0800

    commit 1

---
+++ src/views/po/poentry.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="poentry">
+    POENTRY
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'POEntry',
+  data() {
+    return {
+      selectedBranch: '',
+      branches: [],
+      orders: []
+    }
+  }
+}
+</script>
