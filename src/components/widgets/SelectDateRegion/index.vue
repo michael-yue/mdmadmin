4d34a081ec13957c4cc3280f8f3d28013972b28a
michael-yue <michael_yue@163.com>
2018-10-26 12:30:39 +0800

    commit 1

--- src/components/widgets/SelectDateRegion/index.vue
+++ src/components/widgets/SelectDateRegion/index.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="dialog">
-    <div class="mask"></div>
-    <div class="dialog-content">
-        <h3 class="title">{{ modal.title }}</h3>
-        <p class="text">{{ modal.text }}</p>
-        <div class="btn-group">
-            <div class="btn" @click="cancel">{{ modal.cancelButtonText }}</div>
-            <div class="btn" @click="submit">{{ modal.confirmButtonText }}</div>
-        </div>
-    </div>
+    <el-date-picker
+      v-model="dateRange"
+      :default-time="['00:00:00', '23:59:59']"
+      :picker-options="pickerOptions"
+      type="daterange"
+      format="yyyy 年 MM 月 dd 日"
+      size="small"
+      start-placeholder="开始日期"
+      end-placeholder="结束日期" />
   </div>  
 </template>
 
 <script>
+import { parseTime, getYesterday, getThisPeriodStart, getThisPeriodEnd,
+  getLastPeriodStart, getLastPeriodEnd, getThisMonthStart, getThisMonthEnd,
+  getLastMonthStart, getLastMonthEnd } from '@/utils'
 export default {
   name: 'dialog',
   props: {
@@ -20,9 +23,36 @@ export default {
   },
   data() {
     return {
-      resolve: '',
-      reject: '',
-      promise: '', // 保存promise对象
+      pickerOptions: {
+        shortcuts: [
+          {
+            text: '昨日',
+            onClick(picker) {
+              picker.$emit('pick', [getYesterday(), getYesterday()])
+            }
+          }, {
+            text: '本期',
+            onClick(picker) {
+              picker.$emit('pick', [getThisPeriodStart(), getThisPeriodEnd()])
+            }
+          }, {
+            text: '上期',
+            onClick(picker) {
+              picker.$emit('pick', [getLastPeriodStart(), getLastPeriodEnd()])
+            }
+          }, {
+            text: '本月',
+            onClick(picker) {
+              picker.$emit('pick', [getThisMonthStart(), getThisMonthEnd()])
+            }
+          }, {
+            text: '上月',
+            onClick(picker) {
+              picker.$emit('pick', [getLastMonthStart(), getLastMonthEnd()])
+            }
+          }
+        ]
+      }
     }
   }
 }
